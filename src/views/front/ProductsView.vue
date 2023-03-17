e649a2960e38a756ac8c10982bea8d29945973c8
Kris Tung <ragdoll.washoku89@gmail.com>
2023-03-18 00:01:30 +0800

    修改products banner 圖片寬高

--- src/views/front/ProductsView.vue
+++ src/views/front/ProductsView.vue
@@ -2,8 +2,10 @@
   <div class="products_wrapper">
     <div class="products_banner">
       <div class="products_banner_position">
-        <img src="../../assets/img/products_banner.jpg" alt="" class="img-fluid" />
-        <div class="img_cover"></div>
+        <div class="img_group">
+          <img src="../../assets/img/products_banner.jpg" alt="" />
+          <div class="img_cover"></div>
+        </div>
 
         <div class="banner_text">
           <h2 class="text-primary banner_h2_pc">走進有花的日子裡</h2>
@@ -24,6 +26,17 @@
   position: relative;
 }
 
+.img_group {
+    width: 100%;
+    height: 320px;
+}
+
+.img_group img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
 .img_cover {
   background: rgba(245, 245, 245, 0.2);
   position: absolute;
@@ -35,18 +48,17 @@
 }
 
 .banner_text {
-    position: absolute;
-    top: 50%;
-    transform: translate(-50%,-50%);
-    left: 50%;
-    background: rgba(255,255,255,0.9);
-    border-radius: 8px;
-    padding: 5px;
-    white-space: nowrap;
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  left: 50%;
+  background: rgba(255, 255, 255, 0.9);
+  border-radius: 8px;
+  padding: 5px;
+  white-space: nowrap;
 }
 
-
-.banner_h3_mobile{
+.banner_h3_mobile {
   margin-bottom: 0;
 }
 </style>
